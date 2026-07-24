6bb7a772b0a511be791d63de4c51b723da32e179
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-07-24 15:53:02 +0530

    Codeship: Added solution for Palindrome Partitioning Ii

--- problems/palindrome-partitioning-ii/solution.cpp
+++ problems/palindrome-partitioning-ii/solution.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
-
     int minCut(string s) {
-        int n =s.size();
-        vector<int> dp(n, -1);
+        int n = s.size();
+        vector<int> dp(n+1, 0);
         vector<vector<bool>> dpPal(n, vector<bool>(n, false));
         for (int len = 1; len <= n; len++) {
-
             for (int i = 0; i + len - 1 < n; i++) {
-
                 int j = i + len - 1;
-
                 if (len == 1)
                     dpPal[i][j] = true;
-
                 else if (len == 2)
                     dpPal[i][j] = (s[i] == s[j]);
 
@@ -21,24 +16,19 @@ public:
                     dpPal[i][j] = (s[i] == s[j] && dpPal[i + 1][j - 1]);
             }
         }
-        return solve(s,0, dp, dpPal)-1;
-    }
-    bool isPalind(string& s, int l, int r ){
-        while(l<r) {
-            if(s[l++]!=s[r--]) return false;
-        }
-        return true;
-    }
-    int solve(string s, int i, vector<int>& dp, vector<vector<bool>>& dpPal){
-        int ans= INT_MAX;
-        if(i==s.size()) return 0;
-        if(dp[i]!=-1) return dp[i];
-        for(int end =i;end<s.size();end++){
-            if (dpPal[i][end]){
-                ans = min(ans, 1+ solve(s,end+1,dp,dpPal));
-            }
-        }
-        return dp[i] = ans;
+        for (int i = n - 1; i >= 0; i--) {
+            int ans = 1e9;
+            for (int end = i; end < n; end++) {
 
+                if (dpPal[i][end]) {
+
+                    ans = min(ans, 1 + dp[end + 1]);
+                }
+            }
+
+            dp[i] = ans;
+        }
+
+        return dp[0] - 1;
     }
 };
