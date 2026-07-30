f104715d045b24df4ac0edbe3ad59dffc21be79d
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-07-30 23:26:48 +0530

    Codeship: Added solution for Word Break

--- problems/word-break/solution.cpp
+++ problems/word-break/solution.cpp
@@ -3,12 +3,16 @@ public:
     bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.length();
         vector<bool>dp(n+1, false);
+        unordered_map<char, vector<string>> mp;
+for (auto &word : wordDict)
+    mp[word[0]].push_back(word);
+
         dp[0]=true;
         for(int i=0;i<n;i++){
             if(!dp[i]) continue;
-            for(auto word: wordDict){
+            for(auto word: mp[s[i]]){
                 int size = word.size();
-                if(i+size <=n && s.substr(i,size)==word) dp[i+size] = true;
+                if(i+size <=n && s.compare(i,size,word)==0) dp[i+size] = true;
             }
         }
         return dp[n];
