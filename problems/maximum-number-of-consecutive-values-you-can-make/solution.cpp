f4cf0a065a25f4119b13ac7473c50246a5e0c4a6
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-07-30 11:44:18 +0530

    Codeship: Added solution for Maximum Number Of Consecutive Values You Can Make

--- problems/maximum-number-of-consecutive-values-you-can-make/solution.cpp
+++ problems/maximum-number-of-consecutive-values-you-can-make/solution.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
     int getMaximumConsecutive(vector<int>& coins) {
-        int ans=0;
-        sort(coins.begin(), coins.end());
-        int start=1;
-        if(coins[0]!=1) return 1;
-        ans = 2;
-        int s = start+1;
-        for(int i=1;i<coins.size();i++){
-            int limit = start+ coins[i];
-            while(true){
-                if(s> limit) break;
-                if(s>=coins[i] && s<=limit){
-                    s++;
-                    ans++;
-                } 
-                else if(s < limit && s < coins[i]) return ans;
-            }
-            start=s-1;
-        }
-        return ans;
+    sort(coins.begin(), coins.end());
+      long long reach =0;
+      for(int coin : coins){
+        if(coin > reach+1) break;
+         reach+=coin;
+      }
+      return reach+1;
         
     }
 };
