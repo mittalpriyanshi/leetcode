c2cf477126ad48730e152729cdec2cb0a776daf9
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-07-31 23:59:08 +0530

    Codeship: Added solution for Maximum Profit In Job Scheduling

--- problems/maximum-profit-in-job-scheduling/solution.cpp
+++ problems/maximum-profit-in-job-scheduling/solution.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+vector<int> dp;
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
         int n = startTime.size();
         vector<vector<long long>> jobs(n,vector<long long>(3));
@@ -8,8 +9,29 @@ public:
             jobs[i][1] = endTime[i];
             jobs[i][2] = profit[i];
         }
-        sort(jobs.begin(), jobs.end(), [](vector<vector<long long>>))
+        sort(jobs.begin(), jobs.end());
 
-       
+        vector<long long> starts;
+        for (auto &job : jobs)
+            starts.push_back(job[0]);
+
+        dp.assign(n, -1);
+        return solve(0, jobs, starts);
+    }
+     int solve(int i, vector<vector<long long>> &jobs, vector<long long> &starts) {
+
+        if (i >= jobs.size())
+            return 0;
+
+        if (dp[i] != -1)
+            return dp[i];
+
+        // Find first job whose start >= current end
+        int next = lower_bound(starts.begin(), starts.end(), jobs[i][1]) - starts.begin();
+
+        int take = jobs[i][2] + solve(next, jobs, starts);
+        int skip = solve(i + 1, jobs, starts);
+
+        return dp[i] = max(take, skip);
     }
 };
