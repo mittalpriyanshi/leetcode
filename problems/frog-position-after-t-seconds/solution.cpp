07a65ba701ba9ba91e53d32bc75db5460b12f091
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-08-04 22:46:54 +0530

    Codeship: Added solution for Frog Position After T Seconds

--- problems/frog-position-after-t-seconds/solution.cpp
+++ problems/frog-position-after-t-seconds/solution.cpp
@@ -1,47 +1,57 @@
 class Solution {
 public:
     double frogPosition(int n, vector<vector<int>>& edges, int t, int target) {
-        vector<bool> vis(n+1, false);
-        vector<vector<int>> adj(n+1);
-        vector<int> outDegree(n+1,0);
-        unordered_map<int, vector<int>> times;
-        times[0].push_back(1);
-        vector<vector<int>> children(n+1);
-        for(auto e:edges){
+        if (n == 1) return 1.0;
+
+        vector<vector<int>> adj(n + 1);
+        for (const auto& e : edges) {
             adj[e[0]].push_back(e[1]);
             adj[e[1]].push_back(e[0]);
-            outDegree[e[0]]++;
-            outDegree[e[1]]++;
         }
-        queue<int> q;
-        q.push(1);
-        int tCount=0;
-        while(!q.empty()){
-            int size = q.size();
-            tCount++;
-            while(size--){
-            int p = q.front();
-            vis[p]=true;
-            q.pop();
-            for(auto child : adj[p]){
-                if(outDegree[child]==0 || vis[child]==true) continue;
-                outDegree[child]--;
-                children[p].push_back(child);
-                times[tCount].push_back(child);
-                q.push(child);
+
+        vector<bool> vis(n + 1, false);
+        // Queue stores pair of {node, current_probability}
+        queue<pair<int, double>> q;
+        
+        q.push({1, 1.0});
+        vis[1] = true;
+
+        int time = 0;
+
+        while (!q.empty() && time <= t) {
+            int sz = q.size();
+            
+            while (sz--) {
+                auto [node, prob] = q.front();
+                q.pop();
+
+                // Count unvisited neighbors (children)
+                int children = 0;
+                for (int neighbor : adj[node]) {
+                    if (!vis[neighbor]) children++;
+                }
+
+                // If this is our target node
+                if (node == target) {
+                    // It's correct if we reached target at exact time t,
+                    // OR we reached it earlier and it's a leaf node (children == 0)
+                    if (time == t || (time < t && children == 0)) {
+                        return prob;
+                    }
+                    return 0.0;
+                }
+
+                // Spread probability to unvisited children
+                for (int neighbor : adj[node]) {
+                    if (!vis[neighbor]) {
+                        vis[neighbor] = true;
+                        q.push({neighbor, prob / children});
+                    }
+                }
             }
-            }
+            time++;
         }
-        double ans=1;
-        if(t<=tCount && find(times[t].begin(), times[t].end(), target)== times[t].end()) return 0;
-        else{
-            int tStart=1;
-            while(tStart<=t && tStart<=tCount){
-                ans = ans * (double(1)/double(times[tStart].size()));
-                // if(find(times[tStart].begin(), times[tStart].end(), target) != times[tStart].end()) return ans;
-                tStart++;
-            }
-        }
-        return ans;
+
+        return 0.0;
     }
 };
