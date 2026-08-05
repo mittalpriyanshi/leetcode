6edadd1c4fdc2f610292c4c5cbb669e68e2bc1a3
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-08-05 17:01:42 +0530

    Codeship: Added solution for Critical Connections In A Network

---
+++ problems/critical-connections-in-a-network/solution.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> criticalConnections(int n,
+                                            vector<vector<int>>& connections) {
+        vector<vector<int>> adj(n);
+        for (auto c : connections) {
+            adj[c[0]].push_back(c[1]);
+            adj[c[1]].push_back(c[0]);
+        }
+        vector<bool> vis(n, false);
+        vector<int> startTime(n);
+        vector<int> lowestTime(n);
+        vector<vector<int>> bridges;
+        // source as 0
+        startTime[0] = 1;
+        int runningTime =1;
+        logic(0,0,runningTime,adj,vis,startTime, lowestTime, bridges);
+        return bridges;
+    }
+    void logic(int node, int parent, int runningTime, vector<vector<int>>& adj, vector<bool>& vis, vector<int>& st, vector<int>& lt, vector<vector<int>>& bridges ) {
+        vis[node]=true;
+        st[node] = lt[node]=runningTime;
+        runningTime++;
+        for(auto child : adj[node]){
+            if(child == parent) continue;
+            if(!vis[child]){
+                logic(child,node, runningTime, adj, vis, st,lt,bridges);
+                lt[node] = min(lt[node], lt[child]);
+                if(lt[child] > lt[node]){
+                    bridges.push_back({child,node});
+                }
+            }
+            else lt[node] = min(lt[node], lt[child]);
+        }
+
+    }
+};
