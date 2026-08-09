5e5476b99b91d3b9440a095d790f683067efc485
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-08-10 02:14:05 +0530

    Codeship: Added solution for Minimum Cost For Tickets

--- problems/minimum-cost-for-tickets/solution.cpp
+++ problems/minimum-cost-for-tickets/solution.cpp
@@ -9,14 +9,16 @@ public:
         for(int i=0;i<days.size();i++){
             daysToTravel[days[i]]=true;
         }
-        return solve(days,costs,maxDay,0,1, daysToTravel);
+        vector<int> dp(maxDay + 1, -1);
+        return solve(days,costs,maxDay,0,1, daysToTravel, dp);
     }
-    int solve(vector<int>& days, vector<int>& costs,int maxDay, int total,int d, vector<bool>& dtt){
-        if(d>maxDay) return total;
-        while(!dtt[d]) d++;
-        int cost1 = costs[0] + solve(days,costs,maxDay,total,d+1,dtt);
-        int cost7 = costs[1] + solve(days,costs,maxDay,total,d+7,dtt);
-        int cost30 = costs[2] + solve(days,costs,maxDay,total,d+30,dtt);
-        return total = min(min(cost1, cost7), cost30);
+    int solve(vector<int>& days, vector<int>& costs,int maxDay, int total,int d, vector<bool>& dtt, vector<int>& dp){
+        if(d>maxDay) return 0;
+        while(!dtt[d] && d<maxDay) d++;
+        if(dp[d]!=-1) return dp[d];
+        int cost1 = costs[0] + solve(days,costs,maxDay,total,d+1,dtt,dp);
+        int cost7 = costs[1] + solve(days,costs,maxDay,total,d+7,dtt,dp);
+        int cost30 = costs[2] + solve(days,costs,maxDay,total,d+30,dtt,dp);
+        return dp[d] = min(min(cost1, cost7), cost30);
     }
 };
