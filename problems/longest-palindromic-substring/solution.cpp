434ad6dc1df87068d97fcc178b6a463f2679ccec
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-08-11 03:53:33 +0530

    Codeship: Added solution for Longest Palindromic Substring

--- problems/longest-palindromic-substring/solution.cpp
+++ problems/longest-palindromic-substring/solution.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        int n = s.size();
-        vector<vector<bool>> dp(n, vector<bool>(n,false));
-        string ans = "";
-        ans+=s[0];
-        for(int start=n-1;start>=0;start--){
-            dp[start][start]=true;
-            for(int end=start;end<n;end++){
-                if(s[start]==s[end] && (end-start<=2 || dp[start+1][end-1]==true)){
-                    dp[start][end]=true;
-                    string subs = s.substr(start, end-start+1);
-                    if(subs.size()>ans.size()) ans = subs;
+    std::string longestPalindrome(std::string s) {
+        if (s.length() <= 1) {
+            return s;
+        }
+        
+        int max_len = 1;
+        int start = 0;
+        int end = 0;
+        std::vector<std::vector<bool>> dp(s.length(), std::vector<bool>(s.length(), false));
+        
+        for (int i = 0; i < s.length(); ++i) {
+            dp[i][i] = true;
+            for (int j = 0; j < i; ++j) {
+                if (s[j] == s[i] && (i - j <= 2 || dp[j + 1][i - 1])) {
+                    dp[j][i] = true;
+                    if (i - j + 1 > max_len) {
+                        max_len = i - j + 1;
+                        start = j;
+                        end = i;
+                    }
                 }
             }
         }
-        return ans;
+        
+        return s.substr(start, end - start + 1);
     }
 };
