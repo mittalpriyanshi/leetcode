c3e47f32cb8c3095625a542b11c9cccc822b21b5
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-08-11 03:46:41 +0530

    Codeship: Added solution for Longest Palindromic Substring

--- problems/longest-palindromic-substring/solution.cpp
+++ problems/longest-palindromic-substring/solution.cpp
@@ -3,10 +3,12 @@ public:
     string longestPalindrome(string s) {
         int n = s.size();
         vector<vector<bool>> dp(n, vector<bool>(n,false));
-        string ans="";
-        for(int i=0;i<n;i++) dp[i][i]=true;
+        string ans = "";
+        ans+=s[0];
+        for(int i=0;i<n;i++) 
         for(int start=0;start<n-1;start++){
-            for(int end=start+1;end<n;end++){
+            dp[start][start]=true;
+            for(int end=start;end<n;end++){
                 if(s[start]==s[end] && (end-start<=2 || dp[start+1][end-1]==true)){
                     dp[start][end]=true;
                     string subs = s.substr(start, end-start+1);
