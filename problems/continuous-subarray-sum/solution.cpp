39510d2bd959852011c85ae5af5c2c5935ec79ed
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-08-14 00:31:15 +0530

    Codeship: Added solution for Continuous Subarray Sum

---
+++ problems/continuous-subarray-sum/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int n= nums.size();
+        unordered_map<int,int> mp; // rem, last index
+        int prefix=0;
+        mp[0]=-1;
+        for(int i=0;i<n;i++){
+            prefix += nums[i];
+            int rem = prefix%k;
+            if(mp.find(rem)!= mp.end() && i-mp[rem]>=2) return true;
+            mp[rem]=i;
+        }
+        return false;
+    }
+};
