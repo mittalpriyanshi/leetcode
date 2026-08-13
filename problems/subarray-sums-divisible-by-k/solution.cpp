3e1fbd1683fbe26ee67641ca54da1e60334362a7
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-08-13 18:45:25 +0530

    Codeship: Added solution for Subarray Sums Divisible By K

---
+++ problems/subarray-sums-divisible-by-k/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        mp[0]=1;
+        int n=nums.size();
+        if(n==1) {
+            if (n%5==0) return 1;
+            else return 0;
+        } 
+        vector<int>pre(n);
+        pre[0]=nums[0];
+        for(int i=1;i<n;i++){
+            pre[i]=nums[i]+pre[i-1];
+        }
+        int ans=0;
+        for(int i =0;i<n;i++){
+            if(mp.find(pre[i]%5)!=mp.end()){
+                ans += mp[pre[i]%5];
+                mp[pre[i]%5]++;     
+            }
+            mp[pre[i]]++;
+        }
+        return ans;
+
+    }
+};
