e218d146ac161d466bd85cd15c3fe3f21d428c5d
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-08-14 19:19:14 +0530

    Codeship: Added solution for Permutation Sequence

---
+++ problems/permutation-sequence/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        string s="";
+        for(int i=1;i<=n;i++){
+            s += to_string(i);
+        }
+        for(int i=0;i<k-1;i++){
+           next_permutation(s.begin(), s.end());
+        }
+        return s;
+    }
+};
