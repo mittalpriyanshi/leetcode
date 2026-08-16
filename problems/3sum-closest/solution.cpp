c6d0eee86df007b92a84a1a1ff12c1fee0bcf7ff
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-08-16 15:23:08 +0530

    Codeship: Added solution for 3sum Closest

--- problems/3sum-closest/solution.cpp
+++ problems/3sum-closest/solution.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        int n= nums.size();
-        if(n==3) return nums[0]+nums[1]+nums[2];
         sort(nums.begin(), nums.end());
-        unordered_map<int,int> diff; //diff,sum
-        int right = n-1;
-        for(int i=0;i<n-2;i++){
-            int sum = nums[i];
-            int left=i+1;
-            while(left<right){
-                sum = sum +nums[left] +nums[right];
-                if(sum==target) return sum;
-                diff[abs(sum-target)] = sum;
-                left++;
+        int result = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < nums.size() - 2; i++) {
+            int left = i + 1, right = nums.size() - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (abs(target - sum) < abs(target - result))
+                    result = sum;
+
+                if (sum == target) return target;
+                else if (sum < target) left++;
+                else right--;
             }
         }
-        int minK=INT_MAX;
-        for(auto [k,v]: diff){
-            minK = min(minK,k);
-        }
-        return diff[minK];
 
+        return result;
     }
 };
