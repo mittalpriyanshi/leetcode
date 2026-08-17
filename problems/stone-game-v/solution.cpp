f6aa3ac8f6409afc4d3eebad71cfe41fc3006a6b
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-08-17 12:07:10 +0530

    Codeship: Added solution for Stone Game V

--- problems/stone-game-v/solution.cpp
+++ problems/stone-game-v/solution.cpp
@@ -7,10 +7,13 @@ public:
         for (int i = 1; i < n; i++) {
             prefix[i] = stoneValue[i] + prefix[i - 1];
         }
-        return solve(0, n - 1, prefix);
+        vector<vector<int>> dp(n, vector<int>(n,-1));
+        return solve(0, n - 1, prefix, dp);
+        
     }
-    int solve(int start, int end, vector<int>& prefix) {
+    int solve(int start, int end, vector<int>& prefix, vector<vector<int>>& dp) {
         if(start>=end) return 0;
+        if(dp[start][end]!=-1) return dp[start][end];
         int score =0;
         for (int mid = start; mid < end; mid++) {
            int sumleft = prefix[mid] - (start > 0 ? prefix[start - 1] : 0);
@@ -18,17 +21,17 @@ public:
             
         if(sumleft > sumright){
             //[start....mid | mid+1...end]
-            score = max(score, sumright + solve(mid+1,end,prefix));
+            score = max(score, sumright + solve(mid+1,end,prefix,dp));
         }
         else if(sumleft < sumright){
-            score = max(score, sumleft + solve(start,mid,prefix));
+            score = max(score, sumleft + solve(start,mid,prefix,dp));
         }
         else{
-            score = max({score, sumleft + solve(start, mid, prefix),
-                             sumright + solve(mid + 1, end, prefix)});
+            score = max({score, sumleft + solve(start, mid, prefix,dp),
+                             sumright + solve(mid + 1, end, prefix,dp)});
         }
         }
         
-        return score;
+        return dp[start][end]=score;
     }
 };
