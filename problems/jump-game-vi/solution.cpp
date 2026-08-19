5202b83dea90cde9b55a6c745b1684a67819a4bb
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-08-19 18:24:49 +0530

    Codeship: Added solution for Jump Game Vi

--- problems/jump-game-vi/solution.cpp
+++ problems/jump-game-vi/solution.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
     int maxResult(vector<int>& nums, int k) {
-        int n=nums.size();
-        if(n==1) return nums[0];
-        vector<int> dp(n,INT_MIN); //dp[i]=max score to reach end from index i
-        //top down
-        // dp[0]=nums[0];
-        // dp[1]= dp[0]+nums[1];
-        // for(int i=2;i<n;i++){
-        //     for(int j=i-1;j>=i-k && j>=0;j--){
-        //         dp[i]=max(nums[i]+dp[j], dp[i]);
-        //     }
-        // }
-        // return dp[n-1];
-        dp[0]=nums[0];
-        multiset<int> s;
-        s.insert(dp[0]);
-        for(int i = 1; i < size(nums); i++) {
-        if(i > k) s.erase(s.find(dp[i - k - 1]));  // erase elements from which we cant jump to current index
-        s.insert(dp[i] = *rbegin(s) + nums[i]);    // choose element with max score and jump from that to the current index
+        int n = nums.size();
+        if (n == 1)
+            return nums[0];
+        vector<int> dp(n); // dp[i]=max score to reach end from index i
+        // top down
+        //  dp[0]=nums[0];
+        //  dp[1]= dp[0]+nums[1];
+        //  for(int i=2;i<n;i++){
+        //      for(int j=i-1;j>=i-k && j>=0;j--){
+        //          dp[i]=max(nums[i]+dp[j], dp[i]);
+        //      }
+        //  }
+        //  return dp[n-1];
+        dp[0] = nums[0];
+        deque<int> dq;
+        dq.push_back(0);
+        for(int i=1;i<n;i++){
+            if(dq.front()<i-k) dq.pop_front();
+            dp[i] = nums[i]+dp[dq.front()];
+            while(!dq.empty() && dp[dq.back()] <=dp[i]) dq.pop_back();
+            dq.push_back(i);
+        }
+        return dp[n - 1];
     }
-	return dp[n-1];
-    }
-    
 };
