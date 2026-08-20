1437d3cbf1bda5c3f63884cf3d6645dfdc9a654f
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-08-20 11:04:10 +0530

    Codeship: Added solution for Distribute Elements Into Two Arrays I

---
+++ problems/distribute-elements-into-two-arrays-i/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        int n= nums.size();
+        vector<int> arr1, arr2, res;
+        arr1.push_back(nums[0]);
+        arr2.push_back(nums[1]);
+        for(int i=2;i<n;i++){
+            if(arr1[arr1.size()-1] > arr2[arr2.size()-1]) arr1.push_back(nums[i]);
+            else arr2.push_back(nums[i]);
+        }
+        res = arr1;
+        for(auto x: arr2) res.push_back(x);
+        return res;
+    }
+};
