7d806711115f44fb1fa022a3c6cf5d515e5b5fbf
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-08-22 16:53:47 +0530

    Codeship: Added solution for Number Of Longest Increasing Subsequence

---
+++ problems/number-of-longest-increasing-subsequence/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        vector<int> ways(n, 1);
+        int maxLength = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i] && dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+                    ways[i] = ways[j];
+                } else if (dp[j] + 1 == dp[i]) {
+                    ways[i] += ways[j]; // Add j's ways to our total ways
+                }
+            }
+            maxLength = max(maxLength, dp[i]);
+        }
+       
+        int totalWays = 0;
+        for (int i = 0; i < n; i++) {
+            if (dp[i] == maxLength) {
+                totalWays += ways[i];
+            }
+        }
+
+        return totalWays;
+    }
+};
