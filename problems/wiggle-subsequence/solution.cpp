39e01afa05bdc2b781f9d4dbbc8389fad8039f40
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-08-24 14:00:49 +0530

    Codeship: Added solution for Wiggle Subsequence

---
+++ problems/wiggle-subsequence/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1) return 1;
+        if(n==2 && nums[0]!=nums[1]) return 2;
+        else if(n==2 && nums[0]==nums[1]) return 1;
+        int count=1;
+        int first = nums[0];
+        int i=0;
+        while(i<n-1){
+        int iprev=i;
+        while(i<n-1 && nums[i+1]>=nums[i])
+            i++;
+        //increasing
+        if(i!=iprev) count++;
+        iprev=i;
+        while(i<n-1 && nums[i+1]<=nums[i]) i++; //decreasing
+        if(i!=iprev) count++;
+     }
+     return count;
+    }
+};
