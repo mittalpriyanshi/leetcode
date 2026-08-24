172cd3f3d0fd5a251cbfc1f0869c388d69ff4c53
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-08-24 20:07:21 +0530

    Codeship: Added solution for Combination Sum Iv

--- problems/combination-sum-iv/solution.cpp
+++ problems/combination-sum-iv/solution.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
         int n=nums.size();
-        vector<int> dp(target+1,0);
+        vector<unsigned int> dp(target+1,0);
         dp[0]=1; //dp[i] = number of combinations to make sum i
         //going denomination wise, to make sum s, we'll check all denominations first
         for(int i=1;i<=target;i++){
             for(auto num: nums){
-                if(i-num>=0){
-                    dp[i]+=dp[i-num];
+                if((i-num) >=0){
+                    dp[i] += dp[i-num];
                 }
             }
         }
