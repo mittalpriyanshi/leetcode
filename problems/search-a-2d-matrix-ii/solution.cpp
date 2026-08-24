5911635fad3a02d2697744c37a93675d77ce3c6f
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-08-24 22:26:23 +0530

    Codeship: Added solution for Search A 2d Matrix Ii

--- problems/search-a-2d-matrix-ii/solution.cpp
+++ problems/search-a-2d-matrix-ii/solution.cpp
@@ -3,10 +3,11 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n=matrix.size();
         int m=matrix[0].size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==target) return true;
-            }
+        int r=n-1, c=0;
+        while(r>=0 && c<m){
+            if(matrix[r][c]==target) return true;
+            else if(matrix[r][c]>target) r--;
+            else c++;
         }
         return false;
     }
