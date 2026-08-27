39c34931949672d5de02fd2d1f9388b619cbb7a3
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-08-27 17:11:29 +0530

    Codeship: Added solution for Delete And Earn

--- problems/delete-and-earn/solution.cpp
+++ problems/delete-and-earn/solution.cpp
@@ -1,32 +1,16 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        unordered_map<int,int> points;
-        for(int a:nums) points[a]+=a;
-        set<int>st(nums.begin(), nums.end());
-        int take = 0; 
-        int skip = 0; 
-        int prevNum = -1;
-        for(int num: st){
-            int currPoints = points[num];
-            if(num==prevNum+1){
-                // either skip this and take the max value of take/skip from before to define our new skip state
-                int newSkip = max(take, skip);
-               // or take this and skip the prev
-               int newTake = currPoints + skip;
-               take = newTake;
-                skip = newSkip;
-            }
-            else{
-                //le hi lo fir
-                int newTake = max(take, skip) + currPoints;
-                int newSkip = max(take, skip);
-                
-                take = newTake;
-                skip = newSkip;
-            }
-            prevNum = num;
+        int peak = *max_element(nums.begin(), nums.end());
+        vector<int> dp(peak+1,0);
+        for(int a:nums){
+            dp[a]+=a;
         }
-        return max(skip,take);
+        dp[0]=0;
+        dp[1]=max(dp[0], dp[1]);
+        for(int i=2;i<=peak;i++){
+            dp[i]=max(dp[i-2]+dp[i], dp[i-1]);
+        }
+        return dp[peak];
     }
 };
