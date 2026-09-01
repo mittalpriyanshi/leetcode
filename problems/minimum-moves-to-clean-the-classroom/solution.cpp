c42d3b36e34e75b4caccf5dfa907067d599e87d8
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-09-01 11:52:53 +0530

    Codeship: Added solution for Minimum Moves To Clean The Classroom

--- problems/minimum-moves-to-clean-the-classroom/solution.cpp
+++ problems/minimum-moves-to-clean-the-classroom/solution.cpp
@@ -1,49 +1,57 @@
 class Solution {
 public:
     vector<int> dir = {0, -1, 0, 1, 0};
+    
     int minMoves(vector<string>& classroom, int energy) {
-        int m = classroom.size();
-        int n = classroom[0].size();
+        int rows = classroom.size();       // Renamed from m
+        int cols = classroom[0].size();   // Renamed from n
         int orgEnergy = energy;
-        vector<vector<int>> litter(m, vector<int>(n));
+        vector<vector<int>> litter(rows, vector<int>(cols,-1));
         int startRow, startCol;
         int count = 0;
         
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (classroom[i][j] == 'S') {
-                    startRow = i;
-                     startCol = j;
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(classroom[i][j] == 'S') {
+                   startRow = i, startCol = j;
                 }
-                if (classroom[i][j] == 'L') {
-                    litter[i][j] = count;
-                    count++;
+                if(classroom[i][j] == 'L') {
+                   litter[i][j] = count;
+                   count++;
                 }
             }
         }
-
+        if (count == 0) return 0;
         int fullmask = (1 << count) - 1;
-        queue<vector<int>> q;
-        q.push({startRow, startCol, 0, 0, energy}); // i,j,mask,steps,energy
-
+        
         // vis[row][col][mask] = max_energy_remaining
         // Initialize with -1 to indicate unvisited
-        vector<vector<vector<int>>> vis(m, vector<vector<int>>(n, vector<int>(1 << count, -1)));
+        vector<vector<vector<int>>> vis(rows, vector<vector<int>>(cols, vector<int>(1 << count, -1)));
+        
+        queue<vector<int>> q;
+        q.push({startRow, startCol, 0, 0, energy}); // r, c, mask, steps, energy
         vis[startRow][startCol][0] = energy;
-        while (!q.empty()) {
+        
+        while(!q.empty()){
             auto v = q.front();
             int r = v[0], c = v[1], mask = v[2], s = v[3], e = v[4];
             q.pop();
-            if (mask == fullmask) {
+            
+            if(mask == fullmask){
                 return s;
             }
-            if (e == 0) continue;
-            for (int i = 0; i < 4; i++) {
+            
+            // If out of energy, we can't move further from this state
+            if (e == 0) continue; 
+            
+            for(int i = 0; i < 4; i++){
                 int nr = r + dir[i];
-                int nc = c + dir[i + 1];
-                if (nr >= 0 && nr < m && nc >= 0 && nc < n) {
+                int nc = c + dir[i+1];
+                
+                if(nr >= 0 && nr < rows && nc >= 0 && nc < cols && classroom[nr][nc] != 'X'){
                     int nextMask = mask;
-                    int nextEnergy = e - 1; 
+                    int nextEnergy = e - 1; // Assuming moving ALWAYS costs 1 energy
+                    
                     if(classroom[nr][nc] == 'L'){
                         int pos = litter[nr][nc];
                         nextMask = mask | (1 << pos); // Use a new variable!
@@ -57,7 +65,7 @@ public:
                         vis[nr][nc][nextMask] = nextEnergy;
                         q.push({nr, nc, nextMask, s + 1, nextEnergy});
                     }
-                }
+                }         
             }
         }
         return -1;
