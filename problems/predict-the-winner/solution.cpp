0053fb897806d68241e85f8a44329c5a64600f37
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-09-02 17:01:38 +0530

    Codeship: Added solution for Predict The Winner

--- problems/predict-the-winner/solution.cpp
+++ problems/predict-the-winner/solution.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    bool recurse(int turn, int p1Score, int p2Score, int left, int right, vector<int>& nums) {
-        if(left > right)
-            return p1Score >= p2Score;
-
-        if(!turn) {
-            return recurse(1, p1Score + nums[left], p2Score, left + 1, right, nums) ||
-                   recurse(1, p1Score + nums[right], p2Score, left, right - 1, nums);
-        }
-
-        return recurse(0, p1Score, p2Score + nums[left], left + 1, right, nums) &&
-               recurse(0, p1Score, p2Score + nums[right], left, right - 1, nums);
+    int recurseDiff(int left, int right, vector<int>& nums, vector<vector<int>>& memo) {
+        if(left == right)
+            return nums[left];
+            if(memo[left][right]!=INT_MIN) return memo[left][right];
+        //curr player minuses the opponents best future score from his current score to see the diff
+        int pickLeft = nums[left]- recurseDiff(left+1, right,nums,memo);
+        int pickRight = nums[right]- recurseDiff(left, right-1,nums,memo);
+        return memo[left][right] = max(pickLeft, pickRight);
     }
 
     bool predictTheWinner(vector<int>& nums) {
-        return recurse(0, 0, 0, 0, nums.size() - 1, nums);
+        int n = nums.size();
+        vector<vector<int>> memo(n, vector<int>(n, INT_MIN));
+        return recurseDiff(0, n - 1, nums, memo)>=0;
     }
 };
