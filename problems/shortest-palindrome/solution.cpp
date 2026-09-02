34a82607a75f01e1d4596c9dc30a4ce70bf21db1
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-09-02 16:38:33 +0530

    Codeship: Added solution for Shortest Palindrome

--- problems/shortest-palindrome/solution.cpp
+++ problems/shortest-palindrome/solution.cpp
@@ -1,13 +1,25 @@
 class Solution {
 public:
     string shortestPalindrome(string s) {
-        string rev = s;
-        reverse(rev.begin(), rev.end());
-        for(int i=0;i<s.size();i++){
-            if(s.substr(0,s.length()-i)== rev.substr(i) ){
-                return rev.substr(0,i)+s;
+    int n = s.length();
+       string rev = s;
+       reverse(rev.begin(), rev.end());
+       string temp = s +"#"+rev;
+       vector<int> LPS(temp.length(),0);
+       fillLps(LPS, temp);
+       int lpslen = LPS[temp.length()-1];
+       string res = rev.substr(0, n-lpslen)+s ;
+       return res;
+    }
+    void fillLps(vector<int>& lps, string temp){
+        int t = temp.size();
+        int j=0;
+        for(int i=1;i<t;i++){
+            while(j>0 && temp[i]!= temp[j]){
+                j=lps[j-1];
             }
+            if(temp[i]==temp[j]) j++;
+            lps[i]=j;
         }
-        return rev+s;
     }
 };
