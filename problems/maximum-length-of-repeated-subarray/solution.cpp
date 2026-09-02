4015ee3967c2612c19d8abe2599ecdab7b543339
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-09-02 17:50:44 +0530

    Codeship: Added solution for Maximum Length Of Repeated Subarray

--- problems/maximum-length-of-repeated-subarray/solution.cpp
+++ problems/maximum-length-of-repeated-subarray/solution.cpp
@@ -3,22 +3,18 @@ public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
         int m = nums2.size();
-        vector<vector<int>> dp(n, vector<int>(m,0));
-        if(nums1[0]==nums2[0]) dp[0][0]=1;
-        for(int j=1;j<m;j++){
-            if(nums1[0]==nums2[j]) dp[0][j]=1+dp[0][j-1];
-            else dp[0][j]=dp[0][j-1];
-        }
-        for(int i=1;i<n;i++){
-            if(nums1[i]==nums2[0]) dp[i][0]=1+dp[i-1][0];
-            else dp[i][0]=dp[i-1][0];
-        }
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                if(nums1[i]==nums2[j]) dp[i][j]= dp[i-1][j-1]+1;
-                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        int max_len = 0;
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    max_len = max(max_len, dp[i][j]); 
+                } 
             }
         }
-        return dp[n-1][m-1];
+        
+        return max_len;
     }
 };
