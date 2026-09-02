54642b719ac61b947d57704f1a5966496be8f2aa
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-09-02 17:15:45 +0530

    Codeship: Added solution for Stone Game

--- problems/stone-game/solution.cpp
+++ problems/stone-game/solution.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-    return true;
+        int n = piles.size();
+        vector<int> dp = piles; 
+        for (int i = n - 2; i >= 0; --i) {
+            for (int j = i + 1; j < n; ++j) {
+                int pickLeft = piles[i] - dp[j];
+                int pickRight = piles[j] - dp[j-1];
+                
+                dp[j] = max(pickLeft, pickRight);
+            }
+        }
+        return dp[n - 1] >= 0;
+    
     }
 };
