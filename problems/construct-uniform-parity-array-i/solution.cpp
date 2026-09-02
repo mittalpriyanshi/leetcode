f060b727d46ab3f08ef69cc97115ef747c8d6424
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-09-02 11:01:51 +0530

    Codeship: Added solution for Construct Uniform Parity Array I

--- problems/construct-uniform-parity-array-i/solution.cpp
+++ problems/construct-uniform-parity-array-i/solution.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int n=nums1.size();
-        int countEven=0, countOdd=0;
-        for(int i=0;i<n;i++){
-            if(nums1[i]%2==0) countEven++;
-            if(nums1[i]%2!=0) countOdd++;
-        }
-        if(countEven==n || countOdd==n) return true;
-        else if(countOdd>1 || countEven>0) return true;
-        return false;
+        return true;
     }
 };
