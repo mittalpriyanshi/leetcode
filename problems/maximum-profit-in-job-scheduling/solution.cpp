64c90fb05dc0aeae7b432147082f01f0afebcd5b
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-09-03 21:50:46 +0530

    Codeship: Added solution for Maximum Profit In Job Scheduling

--- problems/maximum-profit-in-job-scheduling/solution.cpp
+++ problems/maximum-profit-in-job-scheduling/solution.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-vector<int> dp;
+vector<long long> dp;
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
-        int n = startTime.size();
+        int n=startTime.size();
         vector<vector<long long>> jobs(n,vector<long long>(3));
         for(int i=0;i<n;i++){
             jobs[i][0] = startTime[i];
@@ -10,28 +10,19 @@ vector<int> dp;
             jobs[i][2] = profit[i];
         }
         sort(jobs.begin(), jobs.end());
-
-        vector<long long> starts;
-        for (auto &job : jobs)
-            starts.push_back(job[0]);
-
-        dp.assign(n, -1);
-        return solve(0, jobs, starts);
+        vector<long long> starts(n);
+        for(int i=0;i<n;i++){
+            starts[i]=jobs[i][0];
+        }
+        dp.assign(n,0);
+        return solve(0, starts, jobs);
     }
-     int solve(int i, vector<vector<long long>> &jobs, vector<long long> &starts) {
-
-        if (i >= jobs.size())
-            return 0;
-
-        if (dp[i] != -1)
-            return dp[i];
-
-        // Find first job whose start >= current end
-        int next = lower_bound(starts.begin(), starts.end(), jobs[i][1]) - starts.begin();
-
-        int take = jobs[i][2] + solve(next, jobs, starts);
-        int skip = solve(i + 1, jobs, starts);
-
-        return dp[i] = max(take, skip);
+    int solve(int i, vector<long long>& starts,  vector<vector<long long>>& jobs){
+        if(i>=starts.size()) return 0;
+        if(dp[i]!=0) return dp[i];
+        int nextStart = lower_bound(starts.begin(), starts.end(), jobs[i][1])-starts.begin();
+        int take = jobs[i][2]+ solve(nextStart, starts, jobs);
+        int skip = solve(i+1,starts,jobs);
+        return dp[i] = max(take,skip);
     }
 };
