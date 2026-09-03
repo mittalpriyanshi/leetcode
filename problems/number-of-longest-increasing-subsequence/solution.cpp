829d690e22705ede7961abf544865e62b4cbe6f5
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-09-03 23:00:03 +0530

    Codeship: Added solution for Number Of Longest Increasing Subsequence

--- problems/number-of-longest-increasing-subsequence/solution.cpp
+++ problems/number-of-longest-increasing-subsequence/solution.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     int findNumberOfLIS(vector<int>& nums) {
-        int n = nums.size();
+        int n=nums.size();
         vector<int> dp(n, 1);
         vector<int> ways(n, 1);
         int maxLength = 1;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i] && dp[j] + 1 > dp[i]) {
-                    dp[i] = dp[j] + 1;
-                    ways[i] = ways[j];
-                } else if (dp[j] + 1 == dp[i]) {
+        dp[0]=1;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j] && dp[j]+1 > dp[i] ){
+                    dp[i] = dp[j]+1;
+                    ways[i]=ways[j];
+                }
+                else if (dp[j] + 1 == dp[i]) {
                     ways[i] += ways[j]; // Add j's ways to our total ways
                 }
             }
             maxLength = max(maxLength, dp[i]);
         }
-       
-        int totalWays = 0;
-        for (int i = 0; i < n; i++) {
-            if (dp[i] == maxLength) {
-                totalWays += ways[i];
-            }
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(dp[i]==maxLength) count+=ways[i];
         }
-
-        return totalWays;
+        return count;
     }
 };
