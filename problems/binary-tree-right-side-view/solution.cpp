1b03f41ea515e0962d354a61f114db44aff627ff
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-09-04 12:59:47 +0530

    Codeship: Added solution for Binary Tree Right Side View

---
+++ problems/binary-tree-right-side-view/solution.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        bfs(root, res);
+        return res;
+        
+    }
+    void bfs(TreeNode* root,vector<int>& res ){
+        if(!root) return;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                auto f = q.front();
+                q.pop();
+                if (i==size-1)  res.push_back(f->val);
+                if(f->left) q.push(f->left);
+                if(f->right) q.push(f->right);
+            }
+        }
+    }
+    
+};
