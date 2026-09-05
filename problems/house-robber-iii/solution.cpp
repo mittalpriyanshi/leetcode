cac3283209f5dcd6fb808ba05b945c89227d937b
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-09-05 14:46:15 +0530

    Codeship: Added solution for House Robber Iii

---
+++ problems/house-robber-iii/solution.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int rob(TreeNode* root) {
+        int l,r;
+        return dfs(root, l,r);
+    }
+    int dfs(TreeNode* root, int& l, int& r){
+        if(!root) return 0;
+        int ll=0,lr=0,rl=0,rr=0;
+        l= dfs(root->left, ll, lr);
+        r= dfs(root->right, rl, rr);
+        return max(root->val + ll+lr+rl+rr, l+r);
+    }
+};
