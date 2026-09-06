84167acd12f8f5db42a5c744bae06b4f5f6093c1
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-09-06 23:02:54 +0530

    Codeship: Added solution for Count Nodes With The Highest Score

---
+++ problems/count-nodes-with-the-highest-score/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+int n;
+int maxScore =0;
+int maxCount=0;
+    int countHighestScoreNodes(vector<int>& parents) {
+        n = parents.size();
+        vector<vector<int>> adj(n);
+        for (int i = 1; i < n; i++) {
+            int parent = parents[i];
+            int child = i;
+            adj[parent].push_back(child);
+        }
+        dfs(0, adj);
+        return maxCount;
+    }
+    int dfs(int parent, vector<vector<int>>& adj) {
+        int currentSubtreeSize = 1; // current node
+        long long currentScore = 1;
+        for (auto child : adj[parent]) {
+            int nodesChild = dfs(child, adj);
+            currentSubtreeSize += nodesChild;
+            currentScore *= nodesChild;
+        }
+        int parentSize = n-currentSubtreeSize;
+        if(parentSize>0) currentScore *= parentSize;
+        if(currentScore >maxScore){
+            maxScore = currentScore;
+            maxCount=1;
+        }
+        else if(currentScore == maxScore) maxCount++;
+        return currentSubtreeSize;
+    }
+};
