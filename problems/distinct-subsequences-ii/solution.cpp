1d0713a0bf5911ead1eeec7a0a292136f6c1df80
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-09-07 12:55:07 +0530

    Codeship: Added solution for Distinct Subsequences Ii

--- problems/distinct-subsequences-ii/solution.cpp
+++ problems/distinct-subsequences-ii/solution.cpp
@@ -10,15 +10,15 @@ const long long MOD = 1e9+7;
         for(int i=1;i<n;i++){
             int c = s[i]-'a';
             if(lastPos[c]!=-1){
-                if(lastPos[c]==0) dp[i]= 2*dp[i-1] ;
-                else dp[i]=2*dp[i-1] - dp[lastPos[c]-1];
+                if(lastPos[c]==0) dp[i]= 2*dp[i-1] % MOD ;
+                else dp[i]=(2*dp[i-1] - dp[lastPos[c]-1])%MOD;
                 lastPos[c]=i;
             }
             else{
-                dp[i]= dp[i-1]*2+1;
+                dp[i]= (dp[i-1]*2)%MOD+1;
                 lastPos[c]=i;
             }
         }
-        return dp[n-1];
+        return dp[n-1]%MOD;
     }
 };
