f44aca463584cba2c3a534fd67c12884afcd455d
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-09-07 18:45:45 +0530

    Codeship: Added solution for Longest Increasing Path In A Matrix

--- problems/longest-increasing-path-in-a-matrix/solution.cpp
+++ problems/longest-increasing-path-in-a-matrix/solution.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
     vector<int> dir = {0, 1, 0, -1, 0};
-    //vector<vector<int>> dp;
+    vector<vector<int>> dp;
     int maxLen = INT_MIN;
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-       // dp.assign(m, vector<int>(n, 0)); // length of longest from that cell
+        int maxLen = INT_MIN;
+        dp.assign(m, vector<int>(n, -1)); // length of longest from that cell
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                vector<vector<bool>> vis(m, vector<bool>(n, false));
-                vis[i][j]=true;
-                dfs(i, j, matrix, m, n,1, vis);
+                maxLen = max(maxLen, dfs(i, j, matrix, m, n));
             }
         }
         return maxLen;
     }
-    void dfs(int i, int j, vector<vector<int>>& matrix, int m, int n, int currLen, vector<vector<bool>> vis) {
+    int dfs(int i, int j, vector<vector<int>>& matrix, int m, int n) {
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        int bestLen = 1;
         for (int d = 0; d < 4; d++) {
             int nr = i + dir[d];
             int nc = j + dir[d + 1];
-            if (nr >= 0 && nr < m && nc >= 0 && nc < n && !vis[nr][nc] && matrix[nr][nc]> matrix[i][j]) {
-                    vis[nr][nc]=true;
-                    dfs(nr,nc,matrix,m,n,currLen+1, vis);
-                    vis[nr][nc]=false;
+            if (nr >= 0 && nr < m && nc >= 0 && nc < n &&
+                matrix[nr][nc] > matrix[i][j]) {
+                bestLen =
+                    max(bestLen, 1 + dfs(nr, nc, matrix, m, n));
             }
         }
-        maxLen = max(maxLen, currLen);
+        return dp[i][j] = bestLen;
     }
 };
