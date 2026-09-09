ff0a0ea8f170ae3f7820226e34421d14b7377113
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-09-09 14:35:59 +0530

    Codeship: Added solution for Count Commas In Range Ii

--- problems/count-commas-in-range-ii/solution.cpp
+++ problems/count-commas-in-range-ii/solution.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     long long countCommas(long long n) {
-        string s = to_string(n);
-        int size = s.size();
-        if(n<1000) return 0;
-        vector<int> commas = {0,0,0,0,1,1,1,2,2,2,3,3,3,4,4,4,5};
-        vector<int> nums(size);
-        long long count=4;
-        long long ans=0;
-        while(count<size){
-            ans +=  (9*(pow(10,count-1)))*commas[count];
-            count++;
+       long long res=0;
+        for(long long p=1000;p<=n;p*=1000){
+            res+=(n-p+1);
         }
-        ans += (n - pow(10,size-1) +1)* commas[size];
-        return ans;
+        return res;
     }
 };
