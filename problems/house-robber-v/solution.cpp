1ddf33a0c84d40d5d0fc2ce8e244896b724acd5c
Priyanshi Mittal <141251647+mittalpriyanshi@users.noreply.github.com>
2026-09-15 23:12:32 +0530

    Codeship: Added solution for House Robber V

--- problems/house-robber-v/solution.cpp
+++ problems/house-robber-v/solution.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     long long rob(vector<int>& nums, vector<int>& colors) {
         int n = nums.size();
-        vector<long long> dp(n + 1, 0);
-        for (int i = n - 1; i >= 0; --i) {
-            bool canrob_both = (i + 1 == n || colors[i] != colors[i + 1]);
-            if (canrob_both) // If we can rob both houses, we do it
-                dp[i] = nums[i] + dp[i + 1];
-            else // If we have to choose, we try both options
-                dp[i] = max((long long)nums[i] + dp[i + 2], dp[i + 1]);
+        long long dp[n + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for (int i = 1; i < n; i++) {
+            if (colors[i] != colors[i - 1]) {
+                dp[i + 1] = dp[i] + nums[i];
+            } else {
+                dp[i + 1] = max(dp[i], dp[i - 1] + nums[i]);
+            }
         }
-
-        return dp[0];
+        return dp[n];
     }
 };
